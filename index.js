ac692b865d18509b75aca8ef8a69307d48149404
WangTZ1996 <2249796929@qq.com>
2019-04-03 16:38:13 +0800

    完成了文案数字与轮播图的对应

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 let mySwiper = new Swiper ('.swiper-container', {
-     autoplay: {
-         stopOnLastSlide: false,
-     },
-     
-    
-    
+  autoplay: {
+    stopOnLastSlide: false,
+  },
+  loop: true,
+  on: {
+    slideChange: function () {
+      getDataNumeber(this.slides[this.activeIndex]);
+    },
+  },
 
-    // 如果需要分页器
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: false,
-      type: 'bullets',
-      bulletElement: 'li',
-      bulletClass : 'pagination',
-      bulletActiveClass: 'paginationActive',
-    },
-    
-    // 如果需要前进后退按钮
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-    // 如果需要滚动条
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  }) 
+  // 如果需要分页器
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
+    bulletElement: 'li',
+    bulletClass : 'pagination',
+    bulletActiveClass: 'paginationActive',
+  },
   
-  mySwiper.pagination.$el.addClass('pagHeight');
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   
-  
+  // 如果需要滚动条
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+function roll(index, count) {
+  // let number = document.getElementsByClassName('number')[index];
+  let num = {
+    value: 0
+  }
+
+  function animate(time) {
+    TWEEN.update(time);
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+
+  var tween = new TWEEN.Tween(num);
+
+  tween.to({ value: count }, 1500);
+  tween.easing(TWEEN.Easing.Quadratic.Out);
+  tween.onUpdate(function () {
+    number.innerHTML = Math.round(num.value) + 'T';
+  });
+  tween.start();
+}
+
+function getDataNumeber(slide) {
+  const span = slide.querySelector('span');
+  span.textContent = span.dataset.number;
+}
